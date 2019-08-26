4b68920a29ae6d759fa4b9228ac53e5a3af1110a
Benedikt Magnus <magnus@magnuscraft.de>
2019-08-26 18:14:00 +0200

    Most minimal GDNative interface

---
+++ include/gdnative/GDNative.pas
@@ -0,0 +1,16 @@
+unit GDNative;
+
+{$mode objfpc}{$H+}
+
+interface
+
+const GodoApiVersion = 1;
+
+type
+    PPGodotObject = ^PGodotObject;
+    PGodotObject = ^TGodotObject;
+    TGodotObject = Pointer;
+
+implementation
+
+end.
